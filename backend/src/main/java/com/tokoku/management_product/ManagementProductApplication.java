fc152df7c9b1641c48f6791053d3b976645d77fc
RAfi123-cmd <rafiulhuda27@gmail.com>
2026-09-01 09:04:22 +0700

    initial commit

---
+++ backend/src/main/java/com/tokoku/management_product/ManagementProductApplication.java
@@ -0,0 +1,13 @@
+package com.tokoku.management_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementProductApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tokoku/management_product/ManagementProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokoku.management_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
